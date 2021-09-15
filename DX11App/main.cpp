a1fdea107d6e9e2964fa60a351e4e7942b99b095
dieseldong <vgvgvvvdc@gmail.com>
2021-09-16 01:14:18 +0800

    更新

--- DX11App/main.cpp
+++ DX11App/main.cpp
@@ -1,8 +1,12 @@
 #include <DirectXMath.h>
 #include <windows.h>
+#include "Common.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	PSTR cmdLine, int showCmd)
 {
+	RE_LOG_INFO("Main", "Hello");
+
+	getchar();
 	return 0;
 }
